7b1d8213565a38dacd2a38ffb2eb8770d4c493e0
tulio <lenientmp99@gmail.com>
2023-10-07 13:10:07 -0300

    feat: minor improvements

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,8 +1,12 @@
-TRAY_POS=right
+killall -q polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [[ $m == "DP-0" ]]; then
+      export TRAY_POS=right
+    fi
     MONITOR=$m polybar --reload top &
   done
 else
   polybar --reload top &
 fi
+unset TRAY_POS
