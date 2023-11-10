644cb5c30d3ea8c09a5d2c0c73e96a61f7d81dcb
tulio <lenientmp99@gmail.com>
2023-11-10 04:57:05 -0300

    fix: remove unused nvim plugins files

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -46,3 +46,9 @@ vim.opt.shortmess:append('c') -- don't give |ins-completion-menu| messages
 vim.opt.iskeyword:append('-') -- hyphenated words recognized by searches
 vim.opt.formatoptions:remove({ 'c', 'r', 'o' }) -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
 vim.opt.runtimepath:remove('/usr/share/vim/vimfiles') -- separate vim plugins from neovim in case vim still in use
+
+-- set node version to asdf
+local home_dir = "/home/tulio"
+local node_bin =  "/.asdf/installs/nodejs/20.7.0/bin"
+vim.g.node_host_prog = home_dir .. node_bin .. "/node"
+vim.cmd("let $PATH = '" .. home_dir .. node_bin .. ":' . $PATH")
