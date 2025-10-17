c9831b7cd5c01032c5b273c2492767a5063bdfd5
Tulio Alcantara <tulioam9@gmail.com>
2025-10-17 00:58:38 -0300

    fix: remove some neovim error messages

--- .config/nvim/lua/core/plugin_config/lsp.lua
+++ .config/nvim/lua/core/plugin_config/lsp.lua
@@ -6,67 +6,149 @@
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-	local opts = { buffer = bufnr }
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-	vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
-	vim.keymap.set('n', '<F3>', vim.lsp.buf.format, opts)
-	vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
-
-	lsp_zero.buffer_autoformat()
-end)
-
-lsp_zero.set_sign_icons({
-	error = '✘',
-	warn = '▲',
-	hint = '⚑',
-	info = '»',
+-- Set up diagnostic signs
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '✘',
+      [vim.diagnostic.severity.WARN] = '▲',
+      [vim.diagnostic.severity.HINT] = '⚑',
+      [vim.diagnostic.severity.INFO] = '»',
+    }
+  }
 })
 
--- Disable formatting for some LSPs, instead use null-ls (prettier) or Biome
-require('lspconfig').ts_ls.setup({
-	on_init = function(client)
-		client.server_capabilities.documentFormattingProvider = false
-		client.server_capabilities.documentFormattingRangeProvider = false
-	end,
+-- Function to set up LSP keymaps when an LSP attaches
+local function on_attach(client, bufnr)
+  local opts = { buffer = bufnr }
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<F3>', vim.lsp.buf.format, opts)
+  vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
+
+  -- Auto format on save
+  if client.supports_method('textDocument/formatting') then
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+    })
+  end
+end
+
+-- LSP server configurations using the new vim.lsp.config API
+vim.lsp.config('ts_ls', {
+  cmd = { 'typescript-language-server', '--stdio' },
+  root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' },
+  on_attach = function(client, bufnr)
+    -- Disable formatting for ts_ls, use prettier instead
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+    on_attach(client, bufnr)
+  end,
 })
-require('lspconfig').cssls.setup({
-	on_init = function(client)
-		client.server_capabilities.documentFormattingProvider = false
-		client.server_capabilities.documentFormattingRangeProvider = false
-	end,
+
+vim.lsp.config('cssls', {
+  cmd = { 'vscode-css-language-server', '--stdio' },
+  root_markers = { 'package.json', '.git' },
+  on_attach = function(client, bufnr)
+    -- Disable formatting for cssls
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+    on_attach(client, bufnr)
+  end,
 })
-require('lspconfig').eslint.setup({
-	on_init = function(client)
-		client.server_capabilities.documentFormattingProvider = false
-		client.server_capabilities.documentFormattingRangeProvider = false
-	end,
+
+vim.lsp.config('eslint', {
+  cmd = { 'vscode-eslint-language-server', '--stdio' },
+  root_markers = { '.eslintrc.js', '.eslintrc.json', '.eslintrc.yaml', '.eslintrc.yml', 'package.json', '.git' },
+  on_attach = function(client, bufnr)
+    -- Disable formatting for eslint
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+    on_attach(client, bufnr)
+  end,
 })
-require('lspconfig').jsonls.setup({
-	on_init = function(client)
-		client.server_capabilities.documentFormattingProvider = false
-		client.server_capabilities.documentFormattingRangeProvider = false
-	end,
+
+vim.lsp.config('jsonls', {
+  cmd = { 'vscode-json-language-server', '--stdio' },
+  root_markers = { 'package.json', '.git' },
+  on_attach = function(client, bufnr)
+    -- Disable formatting for jsonls
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+    on_attach(client, bufnr)
+  end,
 })
-require('lspconfig').volar.setup({
-	on_init = function(client)
-		client.server_capabilities.documentFormattingProvider = false
-		client.server_capabilities.documentFormattingRangeProvider = false
-	end,
-	filetypes = { 'vue' },
-	init_options = {
-		vue = {
-			hybridMode = false,
-		},
-	},
+
+vim.lsp.config('volar', {
+  cmd = { 'vue-language-server', '--stdio' },
+  root_markers = { 'package.json', 'vue.config.js', '.git' },
+  filetypes = { 'vue' },
+  init_options = {
+    vue = {
+      hybridMode = false,
+    },
+  },
+  on_attach = function(client, bufnr)
+    -- Disable formatting for volar
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+    on_attach(client, bufnr)
+  end,
 })
-require('lspconfig').lua_ls.setup(lsp_zero.nvim_lua_ls())
-lsp_zero.setup()
+
+vim.lsp.config('lua_ls', {
+  cmd = { 'lua-language-server' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git' },
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT' },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          "${3rd}/luv/library"
+        }
+      },
+      completion = {
+        callSnippet = "Replace"
+      },
+      diagnostics = {
+        disable = { 'missing-fields' }
+      }
+    }
+  },
+  on_attach = on_attach,
+})
+
+vim.lsp.config('html', {
+  cmd = { 'vscode-html-language-server', '--stdio' },
+  root_markers = { 'package.json', '.git' },
+  on_attach = on_attach,
+})
+
+vim.lsp.config('bashls', {
+  cmd = { 'bash-language-server', 'start' },
+  root_markers = { '.git' },
+  on_attach = on_attach,
+})
+
+vim.lsp.config('glint', {
+  cmd = { 'glint-language-server' },
+  root_markers = { 'package.json', '.git' },
+  on_attach = on_attach,
+})
+
+vim.lsp.config('astro', {
+  cmd = { 'astro-ls', '--stdio' },
+  root_markers = { 'astro.config.mjs', 'astro.config.js', 'package.json', '.git' },
+  on_attach = on_attach,
+})
 
 --------------------------------
 --           MASON            --
@@ -74,21 +156,18 @@ lsp_zero.setup()
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {
-		'ts_ls',
-		'eslint',
-		'lua_ls',
-		'bashls',
-		'jsonls',
-		'cssls',
-		'html',
-		'volar',
-		'glint',
-		'astro',
-	},
-	handlers = {
-		lsp_zero.default_setup,
-	},
+  ensure_installed = {
+    'ts_ls',
+    'eslint',
+    'lua_ls',
+    'bashls',
+    'jsonls',
+    'cssls',
+    'html',
+    'volar',
+    'glint',
+    'astro',
+  },
 })
 
 --------------------------------
@@ -99,28 +178,28 @@ local cmp = require('cmp')
 local lspkind = require('lspkind')
 
 cmp.setup({
-	sources = {
-		{ name = 'copilot' },
-		{ name = 'nvim_lsp' },
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<CR>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = false,
-		}),
-	}),
-	snippet = {
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
-	formatting = {
-		format = lspkind.cmp_format({
-			mode = 'symbol_text',
-			maxwidth = 50,
-			ellipsis_char = '...',
-		}),
-	},
+  sources = {
+    { name = 'copilot' },
+    { name = 'nvim_lsp' },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false,
+    }),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+      maxwidth = 50,
+      ellipsis_char = '...',
+    }),
+  },
 })
 
 --------------------------------
@@ -128,25 +207,22 @@ cmp.setup({
 --------------------------------
 
 local null_ls = require('null-ls')
-local null_opts = lsp_zero.build_options('null-ls', {})
 local mason_null_ls = require('mason-null-ls')
 
 null_ls.setup({
-	on_attach = function(client, bufnr)
-		null_opts.on_attach(client, bufnr)
-	end,
-	sources = {
-		-- null_ls.builtins.formatting.prettierd.with({
-		-- 	env = {
-		-- 		PRETTIERD_DEFAULT_CONFIG = vim.fn.expand('~/.config/nvim/.prettierrc.json'),
-		-- 	},
-		-- }),
-		null_ls.builtins.formatting.stylua,
-	},
+  on_attach = on_attach,
+  sources = {
+    -- null_ls.builtins.formatting.prettierd.with({
+    -- 	env = {
+    -- 		PRETTIERD_DEFAULT_CONFIG = vim.fn.expand('~/.config/nvim/.prettierrc.json'),
+    -- 	},
+    -- }),
+    null_ls.builtins.formatting.stylua,
+  },
 })
 
 mason_null_ls.setup({
-	ensure_installed = {},
-	automatic_installation = true,
-	automatic_setup = true,
+  ensure_installed = {},
+  automatic_installation = true,
+  automatic_setup = true,
 })
--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -1,233 +1,232 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable',
-		lazypath,
-	})
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	--------------------------------
-	--        DECORATION          --
-	--------------------------------
+  --------------------------------
+  --        DECORATION          --
+  --------------------------------
 
-	'lewis6991/gitsigns.nvim',
-	'nvim-tree/nvim-web-devicons',
-	{ 'echasnovski/mini.icons', version = false },
-	'MunifTanjim/nui.nvim',
-	-- 'nvim-tree/nvim-tree.lua',
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = {
-			'meuter/lualine-so-fancy.nvim',
-		},
-	},
-	{
-		'norcalli/nvim-colorizer.lua',
-		config = function()
-			require('colorizer').setup()
-		end,
-	},
-	-- {
-	-- 	'lukas-reineke/indent-blankline.nvim',
-	-- 	config = function()
-	-- 		require('ibl').setup()
-	-- 	end,
-	-- },
-	{
-		'windwp/nvim-ts-autotag',
-		opts = {},
-	},
-	{
-		'goolord/alpha-nvim',
-		config = function()
-			require('alpha').setup(require('alpha.themes.startify').config)
-		end,
-	},
-	{
-		'stevearc/dressing.nvim',
-		opts = {},
-	},
-	{
-		'rebelot/kanagawa.nvim',
-	},
-	{
-		'folke/tokyonight.nvim',
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			require('tokyonight').setup({
-				style = 'night',
-			})
-			vim.cmd('colorscheme tokyonight')
-		end,
-	},
-	-- {
-	-- 	'romgrk/barbar.nvim',
-	-- 	init = function()
-	-- 		vim.g.barbar_auto_setup = false
-	-- 	end,
-	-- 	opts = {},
-	-- },
+  'lewis6991/gitsigns.nvim',
+  'nvim-tree/nvim-web-devicons',
+  { 'echasnovski/mini.icons', version = false },
+  'MunifTanjim/nui.nvim',
+  -- 'nvim-tree/nvim-tree.lua',
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'meuter/lualine-so-fancy.nvim',
+    },
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
+  -- {
+  -- 	'lukas-reineke/indent-blankline.nvim',
+  -- 	config = function()
+  -- 		require('ibl').setup()
+  -- 	end,
+  -- },
+  {
+    'windwp/nvim-ts-autotag',
+    opts = {},
+  },
+  {
+    'goolord/alpha-nvim',
+    config = function()
+      require('alpha').setup(require('alpha.themes.startify').config)
+    end,
+  },
+  {
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+  {
+    'rebelot/kanagawa.nvim',
+  },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require('tokyonight').setup({
+        style = 'night',
+      })
+      vim.cmd('colorscheme tokyonight')
+    end,
+  },
+  -- {
+  -- 	'romgrk/barbar.nvim',
+  -- 	init = function()
+  -- 		vim.g.barbar_auto_setup = false
+  -- 	end,
+  -- 	opts = {},
+  -- },
 
-	--------------------------------
-	--          GIT               --
-	--------------------------------
+  --------------------------------
+  --          GIT               --
+  --------------------------------
 
-	{
-		'kdheepak/lazygit.nvim',
-		init = function()
-			vim.g.lazygit_floating_window_scaling_factor = 1
-		end,
-	},
-	'tpope/vim-fugitive',
-	'sindrets/diffview.nvim',
-	{
-		'APZelos/blamer.nvim',
-		init = function()
-			vim.g.blamer_enabled = false
-		end,
-	},
+  {
+    'kdheepak/lazygit.nvim',
+    init = function()
+      vim.g.lazygit_floating_window_scaling_factor = 1
+    end,
+  },
+  'tpope/vim-fugitive',
+  'sindrets/diffview.nvim',
+  {
+    'APZelos/blamer.nvim',
+    init = function()
+      vim.g.blamer_enabled = false
+    end,
+  },
 
-	--------------------------------
-	--          ACTIONS           --
-	--------------------------------
+  --------------------------------
+  --          ACTIONS           --
+  --------------------------------
 
-	'nvim-lua/plenary.nvim',
-	{
-		'ThePrimeagen/harpoon',
-		config = function()
-			require('harpoon').setup({
-				menu = {
-					width = vim.api.nvim_win_get_width(0) - 4,
-				},
-			})
-		end,
-	},
-	'folke/which-key.nvim',
-	'nvim-pack/nvim-spectre',
-	'mbbill/undotree',
-	{
-		'ibhagwan/fzf-lua',
-		config = function()
-			require('fzf-lua').setup({
-				winopts = {
-					fullscreen = true,
-					preview = {
-						layout = 'vertical',
-					},
-				},
-				files = {
-					formatter = 'path.filename_first',
-				},
-			})
-		end,
-	},
-	{
-		'stevearc/oil.nvim',
-		opts = {},
-	},
-	{
-		'jake-stewart/multicursor.nvim',
-		branch = '1.0',
-	},
-	{
-		'folke/ts-comments.nvim',
-		opts = {},
-		event = 'VeryLazy',
-		enabled = vim.fn.has('nvim-0.10.0') == 1,
-	},
-	{
-		'mikavilpas/yazi.nvim',
-		event = 'VeryLazy',
-		opts = {
-			open_for_directories = true,
-			keymaps = {
-				show_help = '<f1>',
-			},
-			floating_window_scaling_factor = 1,
-			yazi_floating_window_border = 'none',
-		},
-	},
-	--------------------------------
-	--            LSP             --
-	--------------------------------
+  'nvim-lua/plenary.nvim',
+  {
+    'ThePrimeagen/harpoon',
+    config = function()
+      require('harpoon').setup({
+        menu = {
+          width = vim.api.nvim_win_get_width(0) - 4,
+        },
+      })
+    end,
+  },
+  'folke/which-key.nvim',
+  'nvim-pack/nvim-spectre',
+  'mbbill/undotree',
+  {
+    'ibhagwan/fzf-lua',
+    config = function()
+      require('fzf-lua').setup({
+        winopts = {
+          fullscreen = true,
+          preview = {
+            layout = 'vertical',
+          },
+        },
+        files = {
+          formatter = 'path.filename_first',
+        },
+      })
+    end,
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+  },
+  {
+    'jake-stewart/multicursor.nvim',
+    branch = '1.0',
+  },
+  {
+    'folke/ts-comments.nvim',
+    opts = {},
+    event = 'VeryLazy',
+    enabled = vim.fn.has('nvim-0.10.0') == 1,
+  },
+  {
+    'mikavilpas/yazi.nvim',
+    event = 'VeryLazy',
+    opts = {
+      open_for_directories = true,
+      keymaps = {
+        show_help = '<f1>',
+      },
+      floating_window_scaling_factor = 1,
+      yazi_floating_window_border = 'none',
+    },
+  },
+  --------------------------------
+  --            LSP             --
+  --------------------------------
 
-	'nvim-treesitter/nvim-treesitter',
-	-- 'nvim-treesitter/nvim-treesitter-context',
-	'onsails/lspkind.nvim',
-	'simrat39/symbols-outline.nvim',
-	'simrat39/rust-tools.nvim',
-	{ 'windwp/nvim-autopairs', event = 'InsertEnter', opts = {} },
-	{
-		'jay-babu/mason-null-ls.nvim',
-		event = { 'BufReadPre', 'BufNewFile' },
-		dependencies = {
-			'williamboman/mason.nvim',
-			'nvimtools/none-ls.nvim',
-		},
-	},
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			{ 'neovim/nvim-lspconfig' },
-			{ 'williamboman/mason.nvim' },
-			{ 'williamboman/mason-lspconfig.nvim' },
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-		},
-	},
-	{
-		'folke/trouble.nvim',
-		opts = {},
-	},
-	{
-		'L3MON4D3/LuaSnip',
-		version = 'v2.*',
-		build = 'make install_jsregexp',
-	},
+  'nvim-treesitter/nvim-treesitter',
+  -- 'nvim-treesitter/nvim-treesitter-context',
+  'onsails/lspkind.nvim',
+  'simrat39/symbols-outline.nvim',
+  'simrat39/rust-tools.nvim',
+  { 'windwp/nvim-autopairs',  event = 'InsertEnter', opts = {} },
+  {
+    'jay-babu/mason-null-ls.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'williamboman/mason.nvim',
+      'nvimtools/none-ls.nvim',
+    },
+  },
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-nvim-lsp',
+  {
+    'zbirenbaum/copilot-cmp',
+    config = function()
+      require('copilot_cmp').setup()
+    end,
+  },
+  {
+    'folke/trouble.nvim',
+    opts = {},
+  },
+  {
+    'L3MON4D3/LuaSnip',
+    version = 'v2.*',
+    build = 'make install_jsregexp',
+  },
 
-	--------------------------------
-	--            DAP            	--
-	--------------------------------
+  --------------------------------
+  --            DAP            	--
+  --------------------------------
 
-	--------------------------------
-	--            MISC            --
-	--------------------------------
+  --------------------------------
+  --            MISC            --
+  --------------------------------
 
-	{
-		'zbirenbaum/copilot.lua',
-		config = function()
-			require('copilot').setup({
-				panel = {
-					auto_refresh = true,
-				},
-				suggestion = {
-					auto_trigger = true,
-					keymap = {
-						accept = '<Tab>',
-					},
-				},
-			})
-		end,
-	},
-	{ 'aserowy/tmux.nvim', opts = {} },
-	{
-		'iamcco/markdown-preview.nvim',
-		cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-		build = 'cd app && yarn install',
-		init = function()
-			vim.g.mkdp_filetypes = { 'markdown' }
-		end,
-		ft = { 'markdown' },
-	},
+  {
+    'zbirenbaum/copilot.lua',
+    config = function()
+      require('copilot').setup({
+        panel = {
+          auto_refresh = true,
+        },
+        suggestion = {
+          auto_trigger = true,
+          keymap = {
+            accept = '<Tab>',
+          },
+        },
+      })
+    end,
+  },
+  { 'aserowy/tmux.nvim', opts = {} },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
 }
 
 require('lazy').setup(plugins)
